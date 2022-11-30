39a3f7b31276528014efccde276e00e5af5c2677
Habouche khaled Abderrahmène <kk_habouche@esi.dz>
2022-11-30 15:35:03 +0100

    pre-final commit

--- script.js
+++ script.js
@@ -1,46 +1,80 @@
-let img1 = document.getElementById("card-1");
-let img2 = document.getElementById("card-2");
-let img3 = document.getElementById("card-3");
+let rightImage = document.getElementById("img-1");
+let middleImage = document.getElementById("img-2");
+let leftImage = document.getElementById("img-3");
+
+let rightCard = document.getElementById("card-1");
+let middleCard = document.getElementById("card-2");
+let leftCard = document.getElementById("card-3");
+
 
 const rightCardClickedHandler = (e) => {
+  
+  leftImage.removeEventListener("click", leftCardClickedHandler);
+  rightImage.removeEventListener("click", rightCardClickedHandler);
+  
+  rightCard.className = "card middleCard";
+  
+  rightCard.children.item(0).className = "header";
+  rightCard.children.item(1).className = "description";
+  
+  middleCard.className = "card hiddenCard leftCard";
+  
+  middleCard.children.item(0).className = "hiddenHeader";
+  middleCard.children.item(1).className = "hiddenDescription";
+  
+  leftCard.className = "card hiddenCard rightCard";
+  
+  leftCard.children.item(0).className = "hiddenHeader";
+  leftCard.children.item(1).className = "hiddenDescription";
+  
+  const tmp = rightCard;
+  rightCard = leftCard;
+  leftCard = middleCard;
+  middleCard = tmp;
 
-  img3.removeEventListener("click", leftCardClickedHandler);
-  img1.removeEventListener("click", rightCardClickedHandler);
-
-  img1.className = "card middleCard";
-  img2.className = "hiddenCard leftCard";
-  img3.className = "hiddenCard rightCard";
-
-  const tmp = img1;
-  img1 = img3;
-  img3 = img2;
-  img2 = tmp; 
-
-  img3.addEventListener("click", leftCardClickedHandler);
-  img1.addEventListener("click",rightCardClickedHandler);
-
+  const tmp1 = rightImage;
+  rightImage = leftImage;
+  leftImage = middleImage;
+  middleImage = tmp1;
+  
+  leftImage.addEventListener("click", leftCardClickedHandler);
+  rightImage.addEventListener("click", rightCardClickedHandler);
 };
 
-img1.addEventListener("click", rightCardClickedHandler);
 
 const leftCardClickedHandler = (e) => {
 
-  console.log(e);
-
-  img3.removeEventListener("click", leftCardClickedHandler);
-  img1.removeEventListener("click", rightCardClickedHandler);
-
-  img1.className = "hiddenCard leftCard";
-  img2.className = "hiddenCard rightCard";
-  img3.className = "card middleCard";
-
-  const tmp = img3;
-  img3 = img1;
-  img1 = img2;
-  img2 = tmp; 
-
-  img3.addEventListener("click", leftCardClickedHandler);
-  img1.addEventListener("click",rightCardClickedHandler);
+  leftImage.removeEventListener("click", leftCardClickedHandler);
+  rightImage.removeEventListener("click", rightCardClickedHandler);
+  
+  rightCard.className = "card hiddenCard leftCard";
+  
+  rightCard.children.item(0).className = "hiddenHeader";
+  rightCard.children.item(1).className = "hiddenDescription";
+  
+  middleCard.className = "card hiddenCard rightCard";
+  
+  middleCard.children.item(0).className = "hiddenHeader";
+  middleCard.children.item(1).className = "hiddenDescription";
+  
+  leftCard.className = "card middleCard";
+  
+  leftCard.children.item(0).className = "header";
+  leftCard.children.item(1).className = "description";
+  
+  const tmp = leftCard;
+  leftCard = rightCard;
+  rightCard = middleCard;
+  middleCard = tmp;
+  
+  const tmp1 = leftImage;
+  leftImage = rightImage;
+  rightImage = middleImage;
+  middleImage = tmp1;
+  
+  leftImage.addEventListener("click", leftCardClickedHandler);
+  rightImage.addEventListener("click", rightCardClickedHandler);
 };
 
-img3.addEventListener("click", leftCardClickedHandler);
+rightImage.addEventListener("click", rightCardClickedHandler);
+leftImage.addEventListener("click", leftCardClickedHandler);
